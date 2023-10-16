b31aa661af2423a2707c36b474da0096d5d4a538
Aya Muhammad Mansour <aya676670@gmail.com>
2023-10-16 13:09:35 +0300

    service file  get all movies , movies Details and Recommended

--- src/app/services/movies-data.service.ts
+++ src/app/services/movies-data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesDataService {
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
+  
+  getPopular():Observable<any> {
+    return this._httpClient.get('https://api.themoviedb.org/3/movie/popular?api_key=c3e181b664d96dbfc2a8921182cc0c6a&page=1');
+  }
+  getDetails(id:number):Observable<any> {
+    return this._httpClient.get(`https://api.themoviedb.org/3/movie/${id}?api_key=c3e181b664d96dbfc2a8921182cc0c6a`);
+  }
+  getRecommended(movie_id:number):Observable<any> {
+    return this._httpClient.get(`
+    https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=c3e181b664d96dbfc2a8921182cc0c6a`);
+  }
 }
