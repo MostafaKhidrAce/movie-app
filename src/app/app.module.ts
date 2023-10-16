62360a1f2cb4576dd39c5b09f0c17bb18f16a348
Aya Muhammad Mansour <aya676670@gmail.com>
2023-10-16 16:47:17 +0300

    send idfrom card  to detailsMovies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
 import { MoviesModule } from './movies/movies.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -16,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     MoviesModule,
-    SharedModule,HttpClientModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movies/card/card.component.ts
+++ src/app/movies/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +7,10 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-  @Input()movie:any;
-
+  constructor(private _router: Router){}
+    @Input()movie:any;
+  redirectToDetails(id:number){
+   console.log(id);
+  this._router.navigate(['movie-details',id]);
+  }
 }
