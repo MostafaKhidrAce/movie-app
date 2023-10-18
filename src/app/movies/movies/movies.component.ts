6ad6d77ac425bc41eac7497f06b643f739a7ce4a
Mostafa Khidr <mostafa5idr.ace@gmail.com>
2023-10-18 06:12:19 +0300

    handle the home-page movies and wishlist integration

--- src/app/movies/movies/movies.component.ts
+++ src/app/movies/movies/movies.component.ts
@@ -2,6 +2,7 @@ import { MoviesWishlistService } from 'src/app/services/movies-wishlist.service'
 import { Movieface } from './../../interface/movieface';
 import { MoviesDataService } from './../../services/movies-data.service';
 import { Component, Input } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
@@ -9,18 +10,27 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent {
-@Input () movieId !:number
-pagination:boolean=true;
+  @Input() movieId!: number;
+  pagination: boolean = true;
   term: string = "";
   pages: Array<number> = [];
   moviesList!: Array<Movieface>;
-  constructor(private _MoviesDataService: MoviesDataService, private _MoviesWishlist:MoviesWishlistService) {
-    this.pages = new Array(10).fill("").map((ele, index) => index + 1)
-
+  constructor(private _MoviesDataService: MoviesDataService, private _MoviesWishlist: MoviesWishlistService, private router: Router) {
+    this.pages = new Array(10).fill("").map((ele, index) => index + 1);
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && event.url === '/') {
+        this.resetWishlist();
+      }
+    });
   }
   ngOnInit(): void {
     this.getMovies(1);
-    this._MoviesWishlist.displayMoviesList()
+    this._MoviesWishlist.displayMoviesList();
+  }
+  resetWishlist() {
+    this._MoviesWishlist.resetWishlist();
+    this.pagination = true;
+    this.getMovies(1);
   }
   getMovies(page: number) {
     this._MoviesDataService.getPopular(page).subscribe({
@@ -30,12 +40,9 @@ pagination:boolean=true;
       }
     });
   }
-
   searchMovie() {
-
     this._MoviesDataService.getSearchMovie(this.term).subscribe({
       next: (data: any) => {
-
         if (this.term == '') {
           this.getMovies(1);
           this.pagination = true;
@@ -47,22 +54,16 @@ pagination:boolean=true;
     });
   }
   search() {
-
-
     if (this.term) {
       this.pagination=false;
       this._MoviesDataService.getSearchMovie(this.term).subscribe({
         next: (data: any) => {
           this.moviesList = data.results;
-
         },
       });
     } else {
       this.pagination=true;
-
       this.getMovies(1);
     }
   }
-
-
 }
--- src/app/services/movies-wishlist.service.ts
+++ src/app/services/movies-wishlist.service.ts
@@ -1,56 +1,3 @@
-// import { PageNotFoundComponent } from './../shared/page-not-found/page-not-found.component';
-// import { HttpClient } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { BehaviorSubject, Observable } from 'rxjs';
-
-
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class MoviesWishlistService {
-//   counter = new BehaviorSubject<number>(0)
-//   sendToWatchList: any[] = [];
-//   constructor() {
-//     this.getCounter()
-//   }
-//   getMoviesList(item: any) {
-//     this.sendToWatchList.push(item);
-//     localStorage.setItem("moviesList", JSON.stringify(this.sendToWatchList))
-
-//   }
-//   removeItem(item: any) {
-//     const index = this.sendToWatchList.indexOf(item);
-//     this.sendToWatchList.splice(index, 1)
-//     localStorage.setItem("moviesList", JSON.stringify(this.sendToWatchList))
-
-//   }
-
-//   displayMoviesList() {
-//     return this.sendToWatchList;
-//   }
-//   setCounter(counter: number) {
-//     this.counter.next(counter)
-//     localStorage.setItem("moviesListCounter", JSON.stringify(this.sendToWatchList.length))
-
-
-//   }
-//   getCounter(): Observable<any> {
-//     if (localStorage.getItem('moviesList') != null) {
-//       this.sendToWatchList = JSON.parse(localStorage.getItem('moviesList') || '{}')
-//       this.counter.next(this.sendToWatchList.length)
-//       return this.counter
-//     }
-//     this.counter.next(this.sendToWatchList.length)
-
-//     return this.counter
-
-
-
-//   }
-
-// }
-
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -63,6 +10,13 @@ export class MoviesWishlistService {
 
   constructor() {
     this.getCounter();
+  }
+
+  resetWishlist() {
+    this.sendToWatchList = [];
+    this.counter.next(0);
+    localStorage.removeItem("moviesList");
+    localStorage.removeItem("moviesListCounter");
   }
 
   getMoviesList(item: any) {
@@ -87,7 +41,7 @@ export class MoviesWishlistService {
 
   getCounter(): BehaviorSubject<number> {
     if (localStorage.getItem('moviesList') != null) {
-      this.sendToWatchList = JSON.parse(localStorage.getItem('moviesList') || '{}');
+      this.sendToWatchList = JSON.parse(localStorage.getItem('moviesList') || '[]');
       this.counter.next(this.sendToWatchList.length);
       return this.counter;
     }
